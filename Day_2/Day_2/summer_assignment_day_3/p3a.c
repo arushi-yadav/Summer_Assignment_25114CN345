2813209f659ee0cd31c5c2676fc31b8a9093ef90
arushi-yadav <aaruscratch@gmail.com>
2026-06-08 15:54:38 +0530

    added Day 8 program

--- Day_2/Day_2/summer_assignment_day_3/p3a.c
+++ Day_2/Day_2/summer_assignment_day_3/p3a.c
@@ -1,22 +1,25 @@
-#include<stdio.h>
-int main(){
-    int num,i,prime = 1;
-    printf("enter a number:");
-    scanf("%d",&num);
-    if(num <= 1)
-    prime = 0;
+#include <stdio.h>
 
-    for(i =2;i<num;i++){
-        if (num%i== 0){
+int main() {
+    int num, i, prime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num <= 1)
+        prime = 0;
+
+    for(i = 2; i < num; i++) {
+        if(num % i == 0) {
             prime = 0;
             break;
-
         }
     }
+
     if(prime)
-    printf("prime number");
+        printf("Prime Number");
     else
-    printf("not prime number");
+        printf("Not Prime Number");
+
     return 0;
-    
 }
